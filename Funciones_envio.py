b56893f0c9b02172544602bf0613c2623f918e96
Arielss <arielm.sereno.a@gmail.com>
2025-04-16 22:17:58 -0400

    Se establece ya ruta de trabajo, se crea ignore

---
+++ Funciones_envio.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox
+import conexion_db
+
+#def agregar_envio():
+#def mostrar_informacion():
+#def actualizar_informacion():
+#def eliminar_registro():
---
+++ Tkinter.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import Funciones_envio
+
+ventana = tk.Tk()
+ventana.title("Registro de mercancías - Transporte ASA")
+ventana.configure(bg="gray91")
+ventana.attributes("-alpha", 0.9)
+
+ventana.mainloop()
+
+#frames
+
